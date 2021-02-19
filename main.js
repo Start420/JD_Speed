79a8f844fa891e1c382ceef12d6f4af0cb863c33
Kirk <64252519+Start420@users.noreply.github.com>
2021-02-19 11:35:19 +0800

    Update main.js

--- main.js
+++ main.js
@@ -9,52 +9,40 @@ if (!requestScreenCapture()) {
     toastLog("请求截图失败");
     exit();
 };
-
-// watchVideo();
-task();
-look();
-//判断今日已完成是否存在
-function isEnd() {
-    var isEnd = text('今日已完成').findOne(2000);
-    if (isEnd) {
-        return true;
+main();
+function main() {
+    task();
+    //判断任务是否需要执行
+    toastLog("判断任务是否需要执行")
+    var btn;
+    var i = 1;
+    while (btn = findTaskBtn(1)) {
+        doShopTask(btn);
+        toastLog('第' + i + '次看商品完成');
+        if (++i > 100) {
+            break;
+        }
     }
-    return false;
-};
-//看视频
-function watchVideo() {
-    app.launchPackage("com.jd.jdlite")
-    sleep(5000)
-    var centerBtn = desc("看看").findOne().parent();
-    var x = centerBtn.bounds().centerX();
-    var y = centerBtn.bounds().centerY();
-    click(x, y);
-    captureScreen("/sdcard/当前截图.png");
-    sleep(1000)
-    var img = images.read("/sdcard/当前截图.png");
-    var templ = images.read(脚本文件夹 + "看看.png");
-    var p = findImage(img, templ);
-    sleep(800)
-    toastLog(click(p + templ.getWidth() / 2, p + templ.getHeight() / 2));
-    toastLog("视频任务开始");
-    if (isEnd()) {
-        toastLog('识别到今日已完成，结束看视频');
-    } else {
-        toastLog("正在观看小视频")
-        var a
-        for (a = 1; a < 100; a++) {
-            toastLog("按钮存在>>>>\n浏览第" + a + "个视频")
-            sleep(random(15000, 18000))
-            swipe(random(540, 640), height - 400, random(760, 860), random(200, 300), 500)
-            if (id("task_float_fengkong_goon").exists()) {
-                click(id("task_float_fengkong_goon"))
-            };
-            if (isEnd()) {
-                toastLog('识别到今日已完成，结束看视频');
-                break;
-            };
-        };
-    };
+    toastLog('商品任务结束');
+    i = 1;
+    while (btn = findTaskBtn(2)) {
+        doActiveTask(btn);
+        toastLog('第' + i + '次看活动完成');
+        if (++i > 30) {
+            break;
+        }
+    }
+    toastLog('活动任务结束');
+    if (btn = findTaskBtn(3)) {
+        toastLog('正在执行看视频任务');
+        doVideoTask(btn);
+    }
+    toastLog('视频任务结束');
+    sleep(1500)
+    back();
+    console.timeEnd("耗时")
+    exit();
+
 };
 //进入任务列表
 function task() {
@@ -83,54 +71,90 @@ function task() {
         task();
     }
 };
-//看商品
-function look() {
-    sleep(500)
-    toastLog("开始逛商品|活动")
-    click("去赚钱", 0)
-    //准备进入循环体
-    var a, b
-    for (a = 1; a < 65; a++) {
-        toastLog("正在浏览第" + a + "个")
-        sleep(6000)
-        for (b = 1; b < 4; b++) {
-            swipe(random(540, 640), height - 400, random(760, 860), random(200, 300), random(800, 1000))
-            sleep(random(1000, 1500))
-        };
-        back();
-        sleep(800)
-        click("去赚钱", 0)
-        if (isEnd()) {
-            toastLog('识别到今日已完成，结束该任务');
-            break;
-        };
-    };
+//找任务按钮
+function findTaskBtn(type) {
+    var taskBtnGroup = text('去赚钱').untilFind();
+    var btn;
+    taskBtnGroup.forEach(function (item) {
+        if (type == 1 && !item.parent().findByText('逛商品赚金币').empty) {
+            btn = item;
+        } else if (type == 2 && !item.parent().findByText('逛活动赚金币').empty) {
+            btn = item;
+        } else if (type == 3 && !item.parent().findByText('看视频赚金币').empty) {
+            btn = item;
+        }
+    });
+    return btn;
 };
-
-function lookTask() {
-    sleep(500)
-    toastLog("开始逛活动")
-    click("去赚钱", 0)
-    //准备进入循环体
-    var a
-    for (a = 1; a < 15; a++) {
-        toastLog("正在浏览第" + a + "个活动")
-        sleep(9000)
-        for (b = 1; b < 5; b++) {
-            swipe(random(540, 640), height - 400, random(760, 860), random(200, 300), random(800, 1000))
-            sleep(random(800, 1200))
-        };
-        back();
-        sleep(800)
-        click("去赚钱", 0)
-        // toastLog(id("ll_task_bottom_next").findOne().click());
-        if (isEnd()) {
-            toastLog('识别到今日已完成，结束该任务');
-            break;
-        };
-    };
-}
-
+//循环滑动函数(活动|商品专用)
+function mySwipe(num,time) {
+    var xpi = device.width / 2;
+    var ypi = device.height * 2 / 3;
+    var ypi2 = 200;
+    for(var i=0;i<=num;i++) {
+        swipe(xpi, ypi, xpi, ypi2, random(800, 1400 ));
+        sleep(time);
+    }
+    
+};
+//循环滑动函数2(视频专用)
+function mySwipe2(time) {
+    var xpi = device.width / 2;
+    var xpi2 = device.width * 4 / 3;
+    var ypi = device.height * 2 / 3;
+    var ypi2 = random(0,250);
+    swipe(xpi, ypi, xpi2, ypi2, random(800, 1400 ));
+    sleep(time);
+};
+//看一次商品任务
+function doShopTask(shopBtn) {
+    shopBtn.click();
+    sleep(random(3200 , 3800 ));
+    mySwipe(random(5,6),random(800, 1400 ));
+    back();
+    sleep(random(800, 1200 ));
+    swipe(800,200,800,1600,random(300,540));
+};
+//看一次活动任务
+function doActiveTask(activeBtn) {
+    activeBtn.click();
+    sleep(random(4200, 5800 ));
+    mySwipe(random(6,7),random( 800, 1400 ));
+    back();
+    sleep(random(800, 1200 ));
+    swipe(800,200,800,1600,random(300,540));
+};
+function doVideoTask(videoBtn) {
+    videoBtn.click();
+    sleep(1500)
+    var child = idEndsWith('ck').findOne();
+            toastLog('识别文本：'+child.text());
+            click(child.text(),0);
+            //一次看20秒,循环上限为10*50=500次
+            for(var i=1; i< 400; i++) {
+                toastLog("第"+i+"次滑动");
+                sleep(200)
+                mySwipe2(random(18200, 22200 ));
+                if(id("task_float_rl_fengkong_goon").exists())
+                {
+                    log(id("task_float_rl_fengkong_goon").findOne().click());
+                    
+                };
+                if (videoIsEnd()) {
+                    toastLog('识别到今日已完成，结束看视频：');
+                    break;
+                };
+                toastLog(">>检测正常<<")
+            };  
+};
+//判断今日已完成是否存在
+function isEnd() {
+    var isEnd = text('今日已完成').findOne(2000);
+    if (isEnd) {
+        return true;
+    }
+    return false;
+};
 function stopApp() {
     let packageName = currentPackage();
     app.openAppSetting(packageName);
